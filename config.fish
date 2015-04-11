43b7048d384d6a2e26c72413d2e1855218d095d3
Igor Kapkov <igasgeek@me.com>
2015-04-11 09:08:54 +0800

    Remove ~/bin with shitty go binaries

--- config.fish
+++ config.fish
@@ -1,8 +1,8 @@
-set fish_user_paths /usr/local/bin /usr/local/sbin ~/bin $fish_user_paths
+set fish_user_paths /usr/local/bin /usr/local/sbin $fish_user_paths
 
 set -gx ARCHFLAGS -arch x86_64
 set -gx MANWIDTH 80
-set -gx EDITOR vi
+set -gx EDITOR nvim
 set -gx LC_ALL en_US.UTF-8
 set -gx RUBY_GC_HEAP_FREE_SLOTS 600000
 set -gx RUBY_GC_HEAP_GROWTH_FACTOR 1.25
