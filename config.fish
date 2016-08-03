066e1121ce1c2e3f99431ddf1d7069973eb09b0f
Vladimir Smyshlyaev <v@vforvova.ru>
2016-08-03 16:46:45 +0300

    feat: abbreviations

--- config.fish
+++ config.fish
@@ -3,7 +3,11 @@ set PATH $HOME/.rbenv/shims $PATH
 set PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
 
 set FLINE_PATH $HOME/.config/fish/fishline
+set ABBR_PATH $HOME/.config/fish/abbreviations
 source $FLINE_PATH/fishline.fish
+source $ABBR_PATH/git.fish
+source $ABBR_PATH/bundler.fish
+source $ABBR_PATH/nvim.fish
 
 rbenv rehash >/dev/null ^&1
 
